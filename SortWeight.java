2550a5bf36a9b89253dddbcddeb5f858b35df4aa
hppll <hppll@yandex.ua>
2018-04-22 02:05:28 +0300

    Resolved #1

---
+++ SortWeight.java
@@ -0,0 +1,19 @@
+package animal2;
+
+import java.util.Comparator;
+class SortWeight implements Comparator<Animal>
+{
+SortName sn = new SortName();
+
+    public int compare(Animal o1, Animal o2)
+    {
+    	sn.compare(o1.Animal, o2.Animal);
+    	if (o1.Animal.equals(o2.Animal)) {
+    		return o1.Weight - o2.Weight;
+    	}else if (o1.Weight == o2.Weight) {
+    		return o1.Lifetime - o2.Lifetime;
+    	} else {
+    	return o1.Weight - o2.Weight;
+    	}
+    }
+ }
