260e72a341872b177bb1fee0ceb281ca8fb6b312
hppll <hppll@yandex.ua>
2018-04-25 23:15:52 +0300

    Fix1

---
+++ Dispatcher.java
@@ -0,0 +1,16 @@
+package animal2;
+
+import java.util.List;
+
+public class Dispatcher {
+	public static void main(String[] args) {
+		Reader reader = new Reader();
+		String[] fileNames = new String [3];
+		fileNames[0] = "D:\\uni\\javaP\\Animals\\Animal.txt";
+		fileNames[1] = "D:\\uni\\javaP\\Animals\\Animal2.txt";
+		fileNames[2] = "D:\\uni\\javaP\\Animals\\Animal3.txt";
+		reader.getAnimalFromFiles(fileNames);
+		
+	}
+
+}
