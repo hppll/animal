fef60876f322e736a786f218224ada158a1fea65
hppll <hppll@yandex.ua>
2018-05-12 20:06:29 +0300

    Resolved #3

--- animal2/Animal.java
+++ animal2/Animal.java
@@ -12,8 +12,8 @@ public class Animal{
 	        this.Weight = Weight;
 	        this.Lifetime = Lifetime;
 	    }
-    // toString обладают все классы в Java, но по умолчанию он возвращает что-то вроде байт-код к примеру "Animal@1540e19d"
-    // Чтобы он выводил то, что нам необходимо, мы можем его переопределить и выводить то, что нам интересует
+    // toString have all classes in Java, but by default it returns something like a bytecode for example "Animal@1540e19d"
+    // In order to it can output  what we need, we can redefine it and output what we are interested in
     @Override
     public String toString() {
         return this.Number + " " + this.Animal + " " + this.Weight + " " + this.Lifetime;
