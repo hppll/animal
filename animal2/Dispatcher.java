884b246922f8e4deff44a52d42651552bc867b47
hppll <hppll@yandex.ua>
2018-05-12 23:49:40 +0300

    Resolved #6

--- animal2/Dispatcher.java
+++ animal2/Dispatcher.java
@@ -9,9 +9,9 @@ public class Dispatcher {
 	public static void main(String v) throws IOException {
 		String value = v;
 		String[] fileNames = new String [3];
-		fileNames[0] = "input\\Animal.txt";
-		fileNames[1] = "input\\Animal2.txt";
-		fileNames[2] = "input\\Animal3.txt";
+		fileNames[0] = "D:\\uni\\javaP\\Animals\\Animal.txt";
+		fileNames[1] = "D:\\uni\\javaP\\Animals\\Animal2.txt";
+		fileNames[2] = "D:\\uni\\javaP\\Animals\\Animal3.txt";
 		List<Animal> allAnimals = Reader.getAnimalFromFiles(fileNames);
 		if (value.equals("Animal")) {
 			Collections.sort(allAnimals, new SortName());
@@ -21,6 +21,6 @@ public class Dispatcher {
 			Collections.sort(allAnimals, new SortLifetime());
 		}
 		Writer writer = new Writer();
-		writer.main(allAnimals);
+		writer.writer(allAnimals);
 	}
 }
--- animal2/Writer.java
+++ animal2/Writer.java
@@ -9,25 +9,38 @@ import java.util.Collections;
 import java.util.List;
 
 public class Writer {
-	 public static void main(List<Animal> allAnimals) throws IOException {
-			Reader read = new Reader();
+	 public static void writer(List<Animal> allAnimals) throws IOException {
 		 try {
-			 File file = new File("output\\between 11 and 40.txt");// just object
-			 PrintStream fw =  new PrintStream(file);
-			 File file2 = new File("output\\between 11 and 40.txt");
-			 PrintStream fw2 =  new PrintStream(file2);
-			 File file3 = new File("output\\more than 40.txt");
-			 PrintStream fw3 =  new PrintStream(file3);
+			 StringBuilder lessThan10 = new StringBuilder();
+			 StringBuilder between11and40 = new StringBuilder();
+			 StringBuilder moreThan40 = new StringBuilder();
 			 for (Animal anim : allAnimals) {
 				 if(anim.Weight <= 10 ) {
-					 fw.println(anim.toString()); // writing symbols to file
+					lessThan10.append(anim).append("\r\n").toString();
 				 }else if (11 <= anim.Weight && anim.Weight <= 40) {
-					 fw2.println(anim.toString());
-				 }else if (anim.Weight > 40)
-					 fw3.println(anim.toString());
+					 between11and40.append(anim).append("\r\n").toString();
+				 }else {
+					 moreThan40.append(anim).append("\r\n").toString();
+				 }
 			 }
+			 File file = new File("C:\\Users\\User\\eclipse-workspace\\animal2\\src\\animal2\\output\\less_than_10.txt");// just object
+			 PrintStream fw =  new PrintStream(file);
+			 fw.print(lessThan10);
 			 fw.flush();
 			 fw.close();
+			 
+			 File file2 = new File("C:\\Users\\User\\eclipse-workspace\\animal2\\src\\animal2\\output\\between_11_and_40.txt");
+			 PrintStream fw2 =  new PrintStream(file2);
+			 fw2.println(between11and40);
+			 fw2.flush();
+			 fw2.close();
+			 
+			 File file3 = new File("C:\\Users\\User\\eclipse-workspace\\animal2\\src\\animal2\\output\\more_than_40.txt");
+			 PrintStream fw3 =  new PrintStream(file3);
+			 fw3.println(moreThan40);
+			 fw3.flush();
+			 fw3.close();
+			 
 		 }  catch(IOException  e) { }
 		 System.out.print("!");  
 	 }
